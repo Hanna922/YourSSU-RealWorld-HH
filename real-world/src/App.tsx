0ceae0ab41a62bc7ede97ff4bc4b5dbfc74270e0
Hanna922 <hanna.urssu@gmail.com>
2023-05-23 23:04:27 +0900

    feat(router): add router, pages

--- real-world/src/App.tsx
+++ real-world/src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import CreatePage from './pages/CreatePage'
+import SettingsPage from './pages/SettingsPage'
+import ArticlePage from './pages/ArticlePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/login"
+            element={<LoginPage />}
+          />
+          <Route
+            path="/profile"
+            element={<ProfilePage />}
+          />
+          <Route
+            path="/create"
+            element={<CreatePage />}
+          />
+          <Route
+            path="/settings"
+            element={<SettingsPage />}
+          />
+          <Route
+            path="/article"
+            element={<ArticlePage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
