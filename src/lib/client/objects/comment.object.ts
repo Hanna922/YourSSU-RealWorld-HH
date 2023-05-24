0acc216168d85d9df9bbe4c6a56a6ad510ba738a
Hanna922 <hanna.urssu@gmail.com>
2023-05-24 20:03:17 +0900

    fix(lib): apply eslint, prettier

--- src/lib/client/objects/comment.object.ts
+++ src/lib/client/objects/comment.object.ts
@@ -1,9 +1,9 @@
 import { ProfileObject } from './profile.object'
 
 export type CommentObject = {
-    id: number;
-    body: string;
-    createdAt: string;
-    updatedAt: string;
-    author: ProfileObject;
+  id: number
+  body: string
+  createdAt: string
+  updatedAt: string
+  author: ProfileObject
 }
