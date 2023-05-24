4972a8502935881550973d6f32efa991fb47a57d
혀느현스 <hyuns@hyuns.dev>
2023-05-24 22:29:27 +0900

    feat(route): divide router.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,11 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import ProfilePage from './pages/ProfilePage'
-import CreatePage from './pages/CreatePage'
-import SettingsPage from './pages/SettingsPage'
-import ArticlePage from './pages/ArticlePage'
+import { BrowserRouter } from 'react-router-dom'
+import { IndexRouter } from './router'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<HomePage />}
-          />
-          <Route
-            path="/login"
-            element={<LoginPage />}
-          />
-          <Route
-            path="/profile"
-            element={<ProfilePage />}
-          />
-          <Route
-            path="/create"
-            element={<CreatePage />}
-          />
-          <Route
-            path="/settings"
-            element={<SettingsPage />}
-          />
-          <Route
-            path="/article"
-            element={<ArticlePage />}
-          />
-        </Routes>
+        <IndexRouter />
       </BrowserRouter>
     </div>
   )
