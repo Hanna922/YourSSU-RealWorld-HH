f0b479783313103fa8b6d9dec5f0c9dc3ab63e7d
dinohan <dinohan.dev@gmail.com>
2023-05-25 23:57:12 +0900

    feat(article-page): articles api 연결

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,5 +1,28 @@
+import { useQuery } from 'react-query'
+
+import { client } from '@/lib/client'
+
 const ArticlePage = () => {
-  return <></>
+  const { data } = useQuery(['articles'], () =>
+    client.article.list({
+      tag: 'string',
+      author: 'string',
+      favorited: 'string',
+      limit: 3,
+      offset: 3,
+    })
+  )
+
+  return (
+    <>
+      {data?.articles.map((article) => (
+        <div key={article.slug}>
+          <h1>{article.title}</h1>
+          <p>{article.description}</p>
+        </div>
+      ))}
+    </>
+  )
 }
 
 export default ArticlePage
