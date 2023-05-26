84b74db4a94471dd4153477fba31676578bfa9de
Hanna922 <hanna.urssu@gmail.com>
2023-05-26 16:57:20 +0900

    feat(LoginPage): LoginPage 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,50 @@
+import { useState } from 'react'
+
+import { AxiosError } from 'axios'
+import { useForm } from 'react-hook-form'
+import { useMutation } from 'react-query'
+import { useNavigate } from 'react-router'
+
 import { Footer } from '@/components/Footer'
 import { Navbar } from '@/components/Navbar'
+import { postUserLogin } from '@/lib/client/endpoints/user.endpoint'
+
+interface LoginForm {
+  email: string
+  password: string
+}
 
 const LoginPage = () => {
+  const { handleSubmit, register } = useForm<LoginForm>({})
+
+  const [errorMessages, setErrorMessages] = useState<string[]>([])
+  const navigate = useNavigate()
+
+  const { mutate } = useMutation(postUserLogin, {
+    onSuccess: () => {
+      setErrorMessages([])
+      navigate('/')
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 422) {
+          setErrorMessages(
+            Object.entries(error.response.data.errors).map(([key, value]) => `${key} ${value}`)
+          )
+        }
+      }
+    },
+  })
+
+  const submit = async (data: LoginForm) => {
+    mutate({
+      user: {
+        email: data.email,
+        password: data.password,
+      },
+    })
+  }
+
   return (
     <>
       <Navbar />
@@ -9,28 +52,25 @@ const LoginPage = () => {
         <div className="container page">
           <div className="row">
             <div className="col-md-6 offset-md-3 col-xs-12">
-              <h1 className="text-xs-center">Sign up</h1>
+              <h1 className="text-xs-center">Sign in</h1>
               <p className="text-xs-center">
-                <a href="">Have an account?</a>
+                <a href="">Need an account?</a>
               </p>
+              {errorMessages.length !== 0 && (
+                <ul className="error-messages">
+                  {errorMessages.map((message, index) => (
+                    <li key={index}>{message}</li>
+                  ))}
+                </ul>
+              )}
 
-              <ul className="error-messages">
-                <li>That email is already taken</li>
-              </ul>
-
-              <form>
+              <form onSubmit={handleSubmit(submit)}>
                 <fieldset className="form-group">
                   <input
                     className="form-control form-control-lg"
-                    type="text"
-                    placeholder="Your Name"
-                  />
-                </fieldset>
-                <fieldset className="form-group">
-                  <input
-                    className="form-control form-control-lg"
-                    type="text"
+                    type="email"
                     placeholder="Email"
+                    {...register('email')}
                   />
                 </fieldset>
                 <fieldset className="form-group">
@@ -38,9 +78,10 @@ const LoginPage = () => {
                     className="form-control form-control-lg"
                     type="password"
                     placeholder="Password"
+                    {...register('password')}
                   />
                 </fieldset>
-                <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+                <button className="btn btn-lg btn-primary pull-xs-right">Sign in</button>
               </form>
             </div>
           </div>
