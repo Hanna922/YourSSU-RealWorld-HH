0214c045df36ea171bb21955e029db12c408fea0
혀느현스 <hyuns@hyuns.dev>
2023-05-26 16:31:45 +0900

    feat(page): registerPage 구현

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+
+import { AxiosError } from 'axios'
+import { useForm } from 'react-hook-form'
+import { useMutation } from 'react-query'
+
+import { Footer } from '@/components/Footer'
+import { Navbar } from '@/components/Navbar'
+import { postUser } from '@/lib/client/endpoints/user.endpoint'
+
+interface IFormData {
+  username: string
+  email: string
+  password: string
+}
+
+const RegisterPage = () => {
+  const { handleSubmit, register } = useForm<IFormData>({})
+
+  const [errorMessages, setErrorMessages] = useState<string[]>([])
+
+  const { mutate } = useMutation(postUser, {
+    onSuccess: () => {
+      setErrorMessages([])
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 422) {
+          setErrorMessages(
+            Object.entries(error.response.data.errors).map(([key, value]) => `${key} ${value}`)
+          )
+        }
+      }
+    },
+  })
+
+  const submit = async (data: IFormData) => {
+    mutate({
+      user: {
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      },
+    })
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="auth-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-6 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Sign up</h1>
+              <p className="text-xs-center">
+                <a href="">Have an account?</a>
+              </p>
+
+              {errorMessages.length !== 0 && (
+                <ul className="error-messages">
+                  {errorMessages.map((message, index) => (
+                    <li key={index}>{message}</li>
+                  ))}
+                </ul>
+              )}
+
+              <form onSubmit={handleSubmit(submit)}>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="text"
+                    placeholder="Your Name"
+                    {...register('username')}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="email"
+                    placeholder="Email"
+                    {...register('email')}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="password"
+                    placeholder="Password"
+                    {...register('password')}
+                  />
+                </fieldset>
+                <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default RegisterPage
