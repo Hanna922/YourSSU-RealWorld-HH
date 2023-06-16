9aedc437f14507b0bd2122fecf87b81e5048adf9
Hanna922 <hanna.urssu@gmail.com>
2023-06-16 13:56:49 +0900

    fix(getArticles): useGetArticles params 오타 수정

--- src/hooks/useGetArticles.tsx
+++ src/hooks/useGetArticles.tsx
@@ -3,14 +3,14 @@ import { useQuery } from 'react-query'
 import { GetArticlesResponse, getArticles } from '@/lib/client/endpoints/article.endpoint'
 
 export const useGetArticles = (params: {
-  tags?: string
+  tag?: string
   author?: string
   favorited?: string
   limit?: number
   offset?: number
 }) => {
   const { data } = useQuery<GetArticlesResponse>(
-    ['article', params.author, params.tags, params.favorited, params.limit, params.offset],
+    ['article', params.author, params.tag, params.favorited, params.limit, params.offset],
     () => getArticles(params),
     {
       onSuccess: (data) => {},
